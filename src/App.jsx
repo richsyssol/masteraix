1da90b70cefe7a8fb422acf787d6c018a31324f5
YatinDevs <c.yatin727@gmail.com>
2025-08-05 13:12:40 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import Layout from "./Layout/Layout";
+import Home from "./pages/Home/Home";
+import Features from "./pages/Detailpage/Features";
+import Services from "./pages/Services/Service";
+import Bloglist from "./pages/Blog/Blog";
+import BlogDetail from "./pages/Blog/BlogDetail";
+import Contact from "./pages/Contact/Contact";
+import Automate from "./pages/Automate/Automate";
+
+function App() {
+  // http://localhost:5173
+  // http://localhost:5173/ - index
+  // http://localhost:5173/about  - path about
+  // http://localhost:5173/contact  - path contact
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/features/:slug" element={<Features />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/blog" element={<Bloglist />} />
+          <Route path="/blog/:id" element={<BlogDetail />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/automate" element={<Automate />} />
+        </Route>
+      </>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/Automate/Automate.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Automate() {
+  return (
+    <div className="container mx-auto h-screen flex flex-col justify-center bg-slate-100 items-center">
+      <span className="text-9xl font-bold text-gray-700">Masteraix</span>
+      <span className="text-4xl text-gray-700">Automate Your Business!</span>
+    </div>
+  );
+}
+
+export default Automate;
